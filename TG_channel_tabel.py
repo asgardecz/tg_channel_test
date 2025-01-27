e81577ac2a96a4e3e6dbe1fcdd78263a0ef7883c
Ivan <vanonvag@gmail.com>
2025-01-27 15:36:53 +0400

    ver.0.0.2jsonwork

--- TG_channel_tabel.py
+++ TG_channel_tabel.py
@@ -17,7 +17,7 @@ class NoteManager:
 
     def add_company(self, name_company):
         if name_company in self.companys:
-            raise ValueError("Компания с таким названием уже существует.")
+            name_company = name_company
         self.companys[name_company] = Company()
 
     def add_work(self, name_company, place, work):
@@ -46,47 +46,20 @@ class NoteManager:
                 return "Нет добавленных компаний."
             return list(self.companys.keys())
 
-    def save_to_file(self, filename):
-        folder = os.path.dirname(filename)
-        if folder and not os.path.exists(folder):
-            os.makedirs(folder)
-
-
-        with open(filename, 'w', encoding='utf-8') as file:
-            data = {
-                name: [
-                    {
-                        "date": note.date.strftime("%d.%m"),
-                        "place": note.place,
-                        "work": note.work
-                    }
-                    for note in company.notes
-                ]
-                for name, company in self.companys.items()
-            }
-            json.dump(data, file, ensure_ascii=False, indent=4)
-
-
-
-
-
-
     def load_from_file(self, filename):
         if not os.path.exists(filename):
             self.companys = {}
             return
 
-
         try:
             with open(filename, 'r', encoding='utf-8') as file:
-                content = file.read()
-                if not content.strip():
-                    self.companys = {}
-                    return
-
-
+                #content = file.read()
+                #if not content.strip():
+                    #self.companys = {}
+                    #return
 
                 data = json.load(file)
+                print(data)
                 for name, notes in data.items():
                     company = Company()
                     for note_data in notes:
@@ -100,9 +73,35 @@ class NoteManager:
 
 
 
+    def save_to_file(self, filename):
+        folder = os.path.dirname(filename)
+        if folder and not os.path.exists(folder):
+            os.makedirs(folder)
+
+
+        with open(filename, 'w', encoding='utf-8') as file:
+            data = {
+                name: [
+                    {
+                        "date": note.date.strftime("%Y-%m-%d %H:%M:%S.%f"),
+                        "place": note.place,
+                        "work": note.work
+                    }
+                    for note in company.notes
+                ]
+                for name, company in self.companys.items()
+            }
+            print(data)
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+
+
+
+
 if __name__ == '__main__':
     manager = NoteManager()
-    #manager.load_from_file("data/allReport.json")
+    manager.load_from_file("data/allReport.json")
 
     name_company = input('Введите название компании - ')
     manager.add_company(name_company)
